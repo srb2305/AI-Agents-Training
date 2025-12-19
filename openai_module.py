89fe9658de2ae3ef9f70f9e6759e1b30bec2c3b8
Saurabh Sahu <srb.com2112@gmail.com>
2025-12-19 23:00:02 +0530

    seo auditor agent

---
+++ openai_module.py
@@ -0,0 +1,32 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# Create an instance of the OpenAI class
+openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+
+def generate_text_basic(prompt: str, model = "gpt-3.5-turbo", system_prompt: str = "You are a helpfull ai assistant"):
+    response = openai_client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": prompt}
+            ]
+        )
+
+    return response.choices[0].message.content
+
+
+
+def generate_text_with_conversation(messages,model = "gpt-3.5-turbo"):
+    response = openai_client.chat.completions.create(
+        model=model,
+        messages=messages
+        )
+    return response.choices[0].message.content
+
+
